ee987f1c527b668306c9b7e4517dc3b4e1e11630
ahmedsammygeek <ahmedsamigeek@gmail.com>
2024-01-06 08:32:23 +0200

    ...

--- resources/views/site/layouts/header.blade.php
+++ resources/views/site/layouts/header.blade.php
@@ -27,7 +27,7 @@
                         <a class="nav-link" href="{{ route('topics.index') }}">المدونة</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="{{ route('contact') }}">تواصل بنا</a>
+                        <a class="nav-link" href="{{ route('contact') }}">تواصل معنا</a>
                     </li>
                 </ul>
             </div>
