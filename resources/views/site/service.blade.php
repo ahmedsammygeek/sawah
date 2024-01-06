24ee6e8d0b14dc73ded4a933602fdc30459a9ee6
ahmedsammygeek <ahmedsamigeek@gmail.com>
2024-01-06 08:17:41 +0200

    ...

--- resources/views/site/service.blade.php
+++ resources/views/site/service.blade.php
@@ -24,7 +24,7 @@
                 {!!  $service->content !!}
                 <h2>لطلب الخدمة</h2>
                 <button> 
-                    <a href="https://wa.me/+2{{ $data['settings']->whatsapp }}?text=ارغب فى  الاستفسار عن الخدمه : {{ $service->name }} " target="-blank">
+                    <a href="https://wa.me/{{ $data['settings']->whatsapp }}?text=ارغب فى  الاستفسار عن الخدمه : {{ $service->name }} " target="-blank">
                         تواصل عن طريق الواتساب
                     </a>
                 </button>
